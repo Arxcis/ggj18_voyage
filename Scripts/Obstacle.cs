d3879ea94b67166ee0fc19f2eb6b47850687b474
Per-Morten Straume <gamma_ray_per@hotmail.com>
2018-01-26 22:51:13 +0100

    Add scrolling background, start workingon obstacle

---
+++ Scripts/Obstacle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public enum Type
+    {
+        WATER,
+        DUST,
+        DEAD_CELLS,
+        WIND // Unsure if this belongs here
+    }
+
+    [SerializeField]
+    Type type;
+    [SerializeField]
+    float maxDown;
+
+
+    Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.y < -maxDown)
+        {
+            rb.velocity = new Vector3(0.0f, -maxDown, 0.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            switch (type)
+            {
+                case Type.WATER:
+                    other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, -1000.0f, 0.0f));
+                    break;
+                case Type.DEAD_CELLS:
+                    break;
+                case Type.DUST:
+                    break;
+                case Type.WIND:
+                    break;
+            }
+        }
+    }
+}
---
+++ Scripts/PmsMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PmsMove : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            var movement = new Vector3(0.0f, 1.0f, 0.0f);
+            gameObject.GetComponent<Rigidbody>().AddForce(movement * 0.5f);
+        }
+
+    }
+}
